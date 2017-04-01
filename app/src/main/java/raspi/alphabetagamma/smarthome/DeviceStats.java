a1b4a4dd14046a830c49928c5239a1777cdf1676
Shrey Naygandhi <shreynaygandhi@live.com>
2017-04-01 13:27:23 +0530

    Initial commit

---
+++ app/src/main/java/raspi/alphabetagamma/smarthome/DeviceStats.java
@@ -0,0 +1,18 @@
+package raspi.alphabetagamma.smarthome;
+
+/**
+ * Created by shreyngd on 1/4/17.
+ */
+
+public class DeviceStats {
+    private String name;
+    private int status;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
---
+++ app/src/main/java/raspi/alphabetagamma/smarthome/Response.java
@@ -0,0 +1,17 @@
+package raspi.alphabetagamma.smarthome;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shreyngd on 1/4/17.
+ */
+
+public class Response {
+
+    private List<DeviceStats> device=new ArrayList<DeviceStats>();
+
+    public List<DeviceStats> getDevice() {
+        return device;
+    }
+}
